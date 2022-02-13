507019dfea3a04d81e24397891135158b2bf4459
MaggieTheCoder <mdurandt@gmail.com>
2022-02-13 21:16:56 +0000

    added router to navbar and form to AddProperty page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "../styles/App.css";
+import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h2>Surreal Estate</h2>
+      <NavBar />
+
+      <Switch>
+        <Route exact path="/" component={Properties} />
+        <Route exact path="/add-property" component={AddProperty} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/NavBar.css";
+import logo from "../images/logo.png";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img className="navbar-logo" src={logo} alt="logo" />
+      <ul className="navbar-links">
+        <li className="navbar-links-item">
+          <Link className="item" to="/">
+            View Properties{" "}
+          </Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link className="item" to="/add-property">
+            Add a Property
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
